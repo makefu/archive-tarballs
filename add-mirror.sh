a2b2d13141fcd2682fbfe1a76f437d8f3f5dc135
makefu <github@syntax-fehler.de>
2017-10-27 14:42:22 +0200

    add-mirror: make more user friendly

--- add-mirror.sh
+++ add-mirror.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
+## mirrors a nixpkgs url like mirror: , http:, ftp: to archive.org
+# usage: add-mirror.sh URL [ITEM-IDENTIFIER]
+
 set -euf
 . $(command -v slog.sh)
 
-ident=nixos-stockholm-tarballs
+if test ! -e $HOME/.config/ia.conf ;then
+  error "please run 'ia configure' first"
+  exit 1
+fi
+
 p=$(nix-prefetch-url --print-path ${1?please provide url to mirror}| tail -n1)
+ident=${2:-nixos-stockholm-tarballs}
 base=$(basename $p)
 
 if ia list $ident | grep "^$base$"; then
